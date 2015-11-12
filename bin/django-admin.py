c232cee62d4a1b0dee93fb01eb2373faad6719ad
pichurrame <pichurrame@ciencias.unam.mx>
2015-11-11 19:43:10 -0600

    Chuchito please

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/decrayodi/Escritorio/Cultura/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ functional_test/cultura_test.py
@@ -0,0 +1,22 @@
+
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_add_a_track(self):
+        # Alicia visita la aplciacion
+        self.browser.get('http://localhost:8000')
+        self.assertIn('FCsocial', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
